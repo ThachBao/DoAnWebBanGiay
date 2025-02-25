ca44a2d8985a024e592400b475f0df7fc283cce3
Thạch Bảo <thachbao2910@gmail.com>
2025-02-25 10:42:12 +0700

    Add files via upload

---
+++ BangPro.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO Products (ProName, MoTa, Gia, HinhAnh, BanChay, Moi, KhuyenMai, CategoryId)
+VALUES
+('Giày Thể Thao', 'Chạy Bộ', 900000.00, '/Image/Image_Sp/GiayTheThao.jpg', 1, 0, 0, 3),
+('Giày Boots', 'Đi phượt', 1200000.00, '/Image/Image_Sp/GiayBoots.jpg', 0, 1, 0, 1),
+('Giày Nữ Thể Thao', 'Chạy bộ', 600000.00, '/Image/Image_Sp/GiayNuTheThao.jpg', 1, 0, 0, 2),
+('Giày Boots Nữ', 'Đi phượt', 1300000.00, '/Image/Image_Sp/GiayNuBoots.jpg', 0, 0, 1, 2),
+('Giày Sandal Unisex', 'Đi dạo', 500000.00, '/Image/Image_Sp/GiaySandal.jpg', 1, 1, 1, 3),
+('Giày Slip On Unisex', 'Thời trang', 750000.00, '/Image/Image_Sp/GiaySlipOn.jpg', 0, 0, 1, 3),
+('Giày Cao Gót Nữ', 'Phong cách thời trang', 1400000.00, '/Image/Image_Sp/GiayCaoGotNu.jpg', 1, 0, 0, 2),
+('Giày Búp Bê Nữ', 'Mềm mại và thoải mái', 850000.00, '/Image/Image_Sp/GiayBupBeNu.jpg', 0, 1, 0, 2),
+('Giày Thể Thao Nam', 'Chạy bộ và thể hình', 1600000.00, '/Image/Image_Sp/GiayTheThaoNam.jpg', 0, 0, 1, 1),
+('Giày Da Nam', 'Sang trọng và bền bỉ', 2000000.00, '/Image/Image_Sp/GiayDaNam.jpg', 1, 0, 0, 1),
+('Giày Lưu Nữ', 'Thích hợp cho mọi dịp', 1900000.00, '/Image/Image_Sp/GiayLuuNu.jpg', 0, 1, 0, 2);
+
